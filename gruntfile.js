527afd159604df5624ae9e176be0cca14c08f577
yrijoleynik <yurij@ua.fm>
2017-04-14 12:46:52 +0300

    hw-10

---
+++ gruntfile.js
@@ -0,0 +1,57 @@
+﻿module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/src/*.js'],
+        dest: 'js/dist/script.main.js'
+      }  
+    },	
+	uglify: {
+		dist: {
+		src: ['js/dist/script.main.js'],
+		dest: 'js/dist/script.main.js'
+		}
+	},
+    sass: {
+        dist: {
+        files: [{
+        expand: true,
+        cwd: 'styles',
+        src: ['*.scss'],
+        dest: 'css',
+        ext: '.css'
+           }]
+        }
+    },
+    cssmin: {
+        target: {
+            files: [{
+            expand: true,
+            cwd: 'css',
+            src: ['*.css', '!*.min.css'],
+            dest: 'css',
+            ext: '.min.css'
+            }]
+        }
+    },	
+    watch: {
+        sass: {
+        files: ['styles/*.scss'],
+        tasks: ['sass'],
+        },
+	}	
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify'); 
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  
+  grunt.registerTask('default', ['sass']);
+
+};
---
+++ js/src/script.js
@@ -0,0 +1,21 @@
+﻿$(function(){	
+	
+	$(".point").on('click', function(){
+		$(".point").css("background", "url(img/point1.jpg)");
+        $(this).css("background", "url(img/point2.jpg)");
+        }); 
+
+    $(".services__block").hover(
+        function(){
+        $(".services__p", this).css("color", "#f4b60d");
+		$(".block__shadow", this).fadeIn(250).css("display", "block");
+        },
+        function(){
+        $(".services__p", this).css("color", "#fff");
+		$(".block__shadow", this).fadeOut(250).css("display", "block");
+        });		
+	
+})();
+
+
+
